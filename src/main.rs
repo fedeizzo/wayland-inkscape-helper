2421c71c60491cb4e108cf5c9316a1f34e4a0b06
Federico Izzo <federico.izzo99@gmail.com>
2022-05-08 21:55:53 +0200

    update main according to config file modifications

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod clipboard;
+mod config;
 mod keyboard;
 mod svg_handler;
-mod config;
 use std::collections::HashMap;
 use std::sync::mpsc::{channel, Receiver, Sender};
 
-use std::{fs, thread};
+use std::thread;
 
 use clipboard::clipboard::{get_clipboard_content, insert_clipboard_content};
 use evdev::Key;
@@ -19,8 +19,11 @@ use config::config::ConfigParser;
 fn main() {
     let config = ConfigParser::new().expect("Please create a file under $XDG_HOME_CONFIG names inkscape-helper.toml for the configuration");
     let (send, recv): (Sender<KeyboardEvent>, Receiver<KeyboardEvent>) = channel();
-    let mut kb = keyboard::keyboard::Keyboard::new(None);
+    let mut kb = keyboard::keyboard::Keyboard::new(&config.keyboards);
     let mut key_states: HashMap<Key, KeyState> = HashMap::new();
+    let mut last_key: Option<Key> = None;
+    let mut current_key: Option<Key> = None;
+    let mut first_key = true;
 
     let _ = thread::spawn(move || {
         kb.read_event_loop(send);
@@ -28,34 +31,33 @@ fn main() {
     loop {
         match recv.recv() {
             Ok(msg) => {
-                key_states.insert(msg.get_key(), msg.get_state());
+                let key = msg.get_key();
+                let state = msg.get_state();
+                key_states.insert(key, state);
+		if !first_key {
+		    last_key = current_key;
+		}
+                current_key = Some(key);
+		first_key = false;
             }
             _ => {}
         }
-        match key_states.get(&Key::KEY_F) {
-            Some(v) => {
-                if *v == KeyState::Released {
-                    key_states.insert(Key::KEY_F, KeyState::Released);
-                    match get_clipboard_content() {
-                        Some(content) => {
-                            let mut figure = SVGFigure::new(&content);
-                            match figure.as_mut() {
-                                Ok(f) => {
-				    let style = config.config.get(&Key::KEY_F).expect("errore");
-                                    f.apply_style_g_tag(*style);
-                                    let updated = f.to_string();
-                                    println!("inserisco",);
-                                    insert_clipboard_content(&updated);
-                                }
-                                Err(e) => panic!("{}", e),
-                            }
+        if let Some((style_closure, key)) = config.get_style(&last_key, &key_states) {
+            key_states.insert(key, KeyState::Released);
+            match get_clipboard_content() {
+                Some(content) => {
+                    let mut figure = SVGFigure::new(&content);
+                    match figure.as_mut() {
+                        Ok(f) => {
+                            f.apply_style_g_tag(&*style_closure);
+                            let updated = f.to_string();
+                            insert_clipboard_content(&updated);
                         }
-                        _ => {}
+                        Err(e) => eprintln!("{}", e),
                     }
                 }
+                _ => {}
             }
-            _ => {}
         }
-        // println!("{:?}", key_states);
     }
 }
